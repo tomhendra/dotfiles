9a1855728e0c57ac35564ad163954647c308c4ae
Tom Hendra <tom.hendra@outlook.com>
2020-11-21 22:28:19 +0100

    submodules for cccw repo & homebrew alias

--- git/clone-projects.sh
+++ git/clone-projects.sh
@@ -2,6 +2,6 @@
 
 dev="${HOME}/Dev"
 
-# Clone project repos into Dev.
-git clone git@github.com:tomhendra/coursework.git ${dev}/coursework
+# Clone project repos into Dev. Use --recursive to clone submodules.
+git clone git@github.com:tomhendra/coursework.git ${dev}/coursework --recurse-submodules -j8
 git clone git@github.com:tomhendra/personal-site.git ${dev}/personal-site
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,5 +1,5 @@
 # Shortcuts
-alias brewup="brew update && brew cleanup && brew doctor"
+alias brewup="brew upgrade && brew cleanup && brew doctor"
 alias copyssh="pbcopy < ${HOME}/.ssh/id_rsa.pub"
 alias flushdns="dscacheutil -flushcache && sudo killall -HUP mDNSResponder"
 alias pingoo="echo 'Pinging Google' && ping www.google.com";
