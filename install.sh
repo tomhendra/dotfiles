556e76fc18cd2c0697445f4568b81b9a773fcb34
Tom Hendra <tom.hendra@outlook.com>
2021-01-05 11:13:00 +0100

    install.sh order

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ sudo -v
 # Keep-alive: update existing `sudo` time stamp until script has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
-# Install Xcode CLT if required.
+# Install Xcode CLT as required by Homebrew.
 if ! xcode-select --print-path &> /dev/null; then
   echo 'Installing Xcode CLT. Close dialog box once complete...'
   xcode-select --install &> /dev/null
@@ -26,11 +26,7 @@ if test ! $(which brew); then
    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 fi
 
-# Update Homebrew recipes.
-echo 'Updating Homebrew...' 
-  brew update
-
-# Install Node via n-install to ~/.n directory.
+# Install LTS & latest versions of Node via n-install to custom .n directory.
 echo "installing Node via n-install..."
   curl -L https://git.io/n-install | N_PREFIX=${HOME}/.n bash -s -- -y lts latest
 
@@ -46,43 +42,43 @@ echo 'Public key copied to clipboard. Paste it into your GitHub account...'
   pbcopy < ${ssh}/id_rsa.pub
   open 'https://github.com/account/ssh'
 
-# dotfiles path variable to be used throughout the rest of this script.
+# dotfiles path variable.
 dotfiles="${HOME}/.dotfiles"
 
-# Clone dotfiles repo containing additional installation scripts. 
-echo 'Downloading dotfiles repo...'
+# Clone dotfiles repo. 
+echo 'Cloning dotfiles...'
   git clone git@github.com:tomhendra/dotfiles.git ${dotfiles}
 
-# Clone GitHub project repos to ~/Dev directory.
-echo 'Downloading project repos...'
-  mkdir -p ${HOME}/Dev
-  sh ${dotfiles}/git/clone-projects.sh
-
-# Install Homebrew paackges & apps with brew bundle.
+# Install Homebrew packges & apps with brew bundle.
 echo 'Installing Homebrew packages, fonts and applications...'
+  brew update
   brew tap homebrew/bundle
   brew bundle --file=${dotfiles}/Brewfile
   brew cleanup
 
-# Quicklook plugins: remove the quarantine attribute (https://github.com/sindresorhus/quick-look-plugins)
-echo 'Removing quarantine from Quicklook plugins...' 
-  xattr -d -r com.apple.quarantine ${HOME}/Library/QuickLook
-
 # Create symlinks from dotfiles.
-echo 'Creating symlinks...'
+echo 'Creating symlinks from dotfiles...' 
   sh ${dotfiles}/create-symlinks.sh
 
-# reload .zshrc to use of Node via n.
-. ${HOME}/.zshrc
-
-# Install global NPM packages.
-echo 'Installing NPM packages...'
-  sh ${dotfiles}/install-npm-global.sh
+# Quicklook plugins: remove the quarantine attribute (https://github.com/sindresorhus/quick-look-plugins)
+echo 'Removing quarantine attribute from Quicklook plugins...' 
+  xattr -d -r com.apple.quarantine ${HOME}/Library/QuickLook
 
 # Install the Night Owl theme for bat / delta.
 echo 'Configuring bat & delta...'  
   git clone https://github.com/batpigandme/night-owlish "${HOME}/.config/bat/themes/night-owlish"
   bat cache --build
+
+# Install global NPM packages.
+echo 'Installing global npm packages...'
+  # reload .zshrc to use Node & npm via n.
+  . ${HOME}/.zshrc
+  sh ${dotfiles}/install-npm-global.sh
+
+# Create ~/Dev directory & Clone GitHub project repos into it.
+echo 'Cloning GitHub repos into Dev...'
+  mkdir -p ${HOME}/Dev
+  sh ${dotfiles}/git/clone-projects.sh
 
 echo "$(whoami)'s developer environment setup is complete! ✅"
 
