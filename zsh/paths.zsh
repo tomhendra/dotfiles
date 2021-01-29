9943b07d8bce85fac56744f489a61cae83cb6c9f
Tom Hendra <tom.hendra@outlook.com>
2021-01-29 16:09:22 +0100

    Added path for Homebrew

--- zsh/paths.zsh
+++ zsh/paths.zsh
@@ -3,7 +3,8 @@
 # append: path+=('/home/some/bin')
 
 # Use project specific binaries before global ones
-path=('node_modules/.bin:vendor/bin' $path)
+path+=('node_modules/.bin:vendor/bin' $path)
+path+=('/usr/local/sbin' $path)
 
 # export to sub-processes (make it inherited by child processes)
 export PATH
