78d0e6c08abad06b18c778a5865e3ccd78232494
Tom Hendra <tom.hendra@outlook.com>
2021-10-15 22:49:49 +0200

    feat: npm update

--- npm.sh
+++ npm.sh
@@ -1,17 +1,12 @@
 #!/bin/sh
   
 # NPM global packages
-npm i -g @11ty/eleventy
-npm i -g @aws-amplify/cli
+npm i -g @cloudflare/wrangler
 npm i -g @sanity/cli 
-npm i -g aws-cdk
-npm i -g fauna-shell
 npm i -g fkill-cli 
 npm i -g serve 
-npm i -g vercel 
 npm i -g npm-check-updates 
 npm i -g npm-quick-run 
-npm i -g parcel-bundler
 npm i -g semantic-release-cli
 npm i -g supabase
 npm i -g typescript 
