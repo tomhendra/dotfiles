4d5f4a0c4af9b1d23146c99bb04fcc405ed7cf15
Tom Hendra <tom.hendra@outlook.com>
2022-04-07 20:29:29 +0200

    global pkgs

--- global-pkg.sh
+++ global-pkg.sh
@@ -7,6 +7,7 @@ pnpm add -g fkill-cli
 pnpm add -g serve 
 pnpm add -g ngrok
 pnpm add -g npm-quick-run 
+pnpm add -g prisma1
 pnpm add -g semantic-release-cli
 pnpm add -g supabase
 pnpm add -g typescript 
