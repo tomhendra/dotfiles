bf62b7e4a4fc8574939d523fc975eba859f6d87e
Tom Hendra <tom.hendra@outlook.com>
2022-12-10 11:20:28 +0100

    removed some unused globals

--- global_pkg.sh
+++ global_pkg.sh
@@ -1,14 +1,10 @@
 #!/bin/sh
   
 # NPM global packages
-pnpm add -g @sanity/cli
 pnpm add -g fkill-cli
-pnpm add -g serve 
+pnpm add -g serve
 pnpm add -g ngrok
-pnpm add -g npm-quick-run 
-pnpm add -g prisma
 pnpm add -g semantic-release-cli
-pnpm add -g supabase
-pnpm add -g typescript 
+pnpm add -g typescript
 pnpm add -g wrangler
 pnpm add -g yarn
