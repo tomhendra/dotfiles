0f7048c05a645d8042367c4b08daa45179f30cad
Tom Hendra <tom.hendra@outlook.com>
2023-09-29 22:39:04 +0200

    git repos

--- git/get_repos.sh
+++ git/get_repos.sh
@@ -3,24 +3,15 @@
 username="tomhendra"
 dir="${HOME}/Developer"
 
-repos_array=(
-  "blog-content"
-  "coursework"
-  "tomhendra.dev"
-  "stitch"
-  "the-lab"
-)
+declare -a repos=("coursework" "tapeo" "the-lab" "tomhendra.dev") 
 
 clone_repo () {
   repo="$1"
   
-  git clone git@github.com:${username}/${i}.git ${dir}/${i} && echo ${i} cloned to ${dir}/${i}"
+  git clone git@github.com:${username}/${i}.git ${dir}/${i} && echo "${i} cloned to ${dir}/${i}"
 }
 
-for i in "${repos_array[@]}";
+for i in "${repos[@]}"
 do
   clone_repo ${i}
 done
-
-# ! /Users/tom/.dotfiles/git/get_repos.sh: line 20: unexpected EOF while looking for matching `"'
-# ! /Users/tom/.dotfiles/git/get_repos.sh: line 24: syntax error: unexpected end of file
