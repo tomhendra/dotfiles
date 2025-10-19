861e812f5d33d1593bde39fd27bc66023bbfe0bd
Tom Hendra <tom.hendra@outlook.com>
2025-10-19 15:29:32 +0200

    feat: update repos list

--- git/get_repos.sh
+++ git/get_repos.sh
@@ -3,7 +3,7 @@
 username="tomhendra"
 dir="${HOME}/Developer"
 
-declare -a repos=("courses" "tapeo" "tomhendra.dev" "tomkit")
+declare -a repos=("courses" "recilla" "tomhendra.dev")
 
 clone_repo () {
   repo="$1"
